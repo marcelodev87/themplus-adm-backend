29c3fa5aba66276ac3c0b9c9f2ae3a1650065f74
Carlos Davi <carlosdavi004@gmail.com>
2025-03-17 23:00:21 -0300

    Feat: Aplicado mais configurações no projeto backend

---
+++ app/Models/Register.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\HasUuid;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+class Register extends Model
+{
+    use HasUuid, Notifiable;
+
+    protected $table = 'registers';
+
+    protected $fillable = [
+        'user_id',
+        'action',
+        'target',
+        'identification',
+        'date_register',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
