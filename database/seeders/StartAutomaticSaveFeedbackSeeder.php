86d2988ad379e2513a7ac3c27b359902540f8020
Ednardo Gomes <ednardogomesdev@gmail.com>
2025-06-24 09:50:54 -0300

    feat: added start seeder for auto-save feedback setting

---
+++ database/seeders/StartAutomaticSaveFeedbackSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Internal\Setting;
+use Illuminate\Database\Seeder;
+
+class StartAutomaticSaveFeedbacktionSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Setting::create([
+            'key' => 'allow_feedback_saved',
+            'value' => '0'
+        ]);
+    }
+}
